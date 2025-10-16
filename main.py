17fc8872ea83c4d293c46ae154b727cafa005612
to-sasaki333 <107777777+to-sasaki333@users.noreply.github.com>
2025-10-16 15:51:33 +0900

    shuusei

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from diaries.DiarySample import DiarySample
-<<<<<<< HEAD
+
 from diaries.Miyazawadiary import Miyazawadiary
 from diaries.SasakiDiary import SasakiDiary
 from diaries.RyoDiary import RyoDiary
@@ -8,13 +8,7 @@ diaries = [DiarySample(),
            Miyazawadiary(),
            SasakiDiary(),
            RyoDiary()
-=======
-from diaries.RyoDiary import RyoDiary
-# ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), 
-           RyoDiary(),
->>>>>>> 4ed7c8da5906587a1657ae62f1d380da3c840ed4
-        ]
+] 
 
 for d in diaries:
     print("---------------------------------")
