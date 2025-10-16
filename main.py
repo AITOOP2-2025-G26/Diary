8d104830a6eaaf74f7410956e48f74fc1ca84bbe
浅野僚介 <ruidora1529@gmail.com>
2025-10-16 15:19:16 +0900

    りょう日記追加

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from diaries.DiarySample import DiarySample
 from diaries.Miyazawadiary import Miyazawadiary
 from diaries.SasakiDiary import SasakiDiary
+from diaries.RyoDiary import RyoDiary
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [DiarySample(),
            Miyazawadiary(),
            SasakiDiary(),
+           RyoDiary()
         ]
 
 for d in diaries:
